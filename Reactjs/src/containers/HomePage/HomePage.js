5e73a0d1e6f7e3937a8c778e006458cf7b181c98
chung <ndanh675@gmai.com>
2025-02-13 16:04:16 +0700

    13/2

--- Reactjs/src/containers/HomePage/HomePage.js
+++ Reactjs/src/containers/HomePage/HomePage.js
@@ -5,7 +5,8 @@ import Speacialty from "./Section/Specialty";
 import MedicalFacility from "./Section/MedicalFacility";
 import OutStandingDoctor from "./Section/OutStandingDoctor";
 import HandBook from "./Section/HandBook";
-
+import HomeFooter from "./Homefooter";
+import About from "./Section/About";
 import "./HomePage.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -25,6 +26,8 @@ class HomePage extends Component {
         <MedicalFacility settings={settings} />
         <OutStandingDoctor settings={settings} />
         <HandBook settings={settings} />
+        <About />
+        <HomeFooter />
       </div>
     );
   }
